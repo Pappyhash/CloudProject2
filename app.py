0a75a2970d604570dfe331850819cf849f3f8d38
Elijah Park <Pappyhash426@gmail.com>
2016-03-18 23:15:25 -0400

    working

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import xmltodict
 import urllib
 import urllib.parse
 from pymongo import MongoClient
+import datetime
+from datetime import date, timedelta
 
 client = MongoClient('mongodb://cloud:cloud@ds015899.mlab.com:15899/cloud-p2')
 db = client.get_default_database()
@@ -22,12 +24,14 @@ def get_prediction(symbol, startDate, endDate):
 	print(symbol, startDate, endDate)
 	startDoc = db['app'].find_one({ 'symbol': symbol, 'date': startDate + 'T00:00:00' })
 	if startDoc is None:
-		lookupStockHistory(symbol, startDate, endDate)
+		if (lookupStockHistory(symbol, startDate, endDate) == 'Error'):
+			return 'Error'
 		startDoc = db['app'].find_one({ 'symbol': symbol, 'date': startDate + 'T00:00:00' })
 
 	endDoc = db['app'].find_one({ 'symbol': symbol, 'date': endDate + 'T00:00:00' })
 	if endDoc is None:
-		lookupStockHistory(symbol, startDate, endDate)
+		if (lookupStockHistory(symbol, startDate, endDate) == 'Error'):
+			return 'Error'
 		endDoc = db['app'].find_one({ 'symbol': symbol, 'date': endDate + 'T00:00:00' })
 
 	print(startDoc)
@@ -68,49 +72,51 @@ def lookupStockHistory(symbol, startDate, endDate):
 	input['StartDate'] = startDate + 'T00:00:00-00'
 	input['EndDate'] = endDate + 'T00:00:00-00'
 
-	print(input)
-
 	url = url + urllib.parse.quote(json.dumps(input))
 
 	r = requests.get(url)
 	result = json.loads(r.text)
 
-	print(result)
+	if (result['Elements'] == []):
+		return 'Error'
+
 	dates = result['Dates']
 	prices = result['Elements'][0]['DataSeries']['close']['values']
+		
 	for i in range(0, len(dates)):
 		store_data_point(symbol, dates[i], prices[i])
 
 	return r.text
 
-@app.route('/predict')
-def predict():
-	symbol = request.args.get('Symbol')
-	startDate = request.args.get('StartDate')
-	endDate = request.args.get('EndDate')
-	print(startDate)
-	print(endDate)
-	result = get_prediction(symbol, startDate, endDate)
-	return json.dumps({ 'result': result })
-
 @app.route('/', methods=['GET', 'POST'])
 def root():
 	if request.method == 'POST':
 		company = request.form['company']
-		result = lookupSymbol(company)
-		return result
+		symbol = lookupSymbol(company)
+		startDate = "2016-01-04"
+		endDate = date.today()
+		while(endDate.isoweekday() < 6):
+			endDate = endDate - timedelta(days=1)
+		prediction = get_prediction(symbol, '2016-01-04', '2016-03-18')
+
+		if prediction == 'Error':
+			template = render_template('error.html', symbol=symbol)
+			return template
+		
+		template = render_template('prediction.html', prediction=prediction, company=company)
+		return template
 	else:
 		return render_template('index.html')
 
 def lookupSymbol(company):
 	parsed_json = json.loads(wrap_get_request('http://dev.markitondemand.com/Api/v2/Lookup', { 'input': company }))
-	return parsed_json['LookupResultList']['LookupResult'][0]['Symbol']
+	if (isinstance(parsed_json['LookupResultList']['LookupResult'], list) == False):
+		return parsed_json['LookupResultList']['LookupResult']['Symbol']
+	else:
+		return parsed_json['LookupResultList']['LookupResult'][0]['Symbol']
 
 def quote():
 	return wrap_get_request('http://dev.markitondemand.com/Api/v2/Quote', { 'symbol': request.args.get('symbol') })
-
-# def lookupStockHistory():
-# 	return lookupStockHistory(request.args.get('Company'), request.args.get('StartDate'), request.args.get('EndDate'))
 
 if __name__ == '__main__':
 	app.run(debug=True,port=8080,host='0.0.0.0')
